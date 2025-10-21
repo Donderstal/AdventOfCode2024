b1b7f207e01f584fd7e20e685c9bf59121d74908
DWF <dwfonderstal@pm.me>
2025-10-21 20:17:09 +0200

    pseudocode and unittest for part 2

--- AdventOfCode2024.UnitTests/Day1UnitTests.cs
+++ AdventOfCode2024.UnitTests/Day1UnitTests.cs
@@ -1,12 +1,20 @@
 ﻿namespace AdventOfCode2024.UnitTests;
 
-public class Tests
+public class Day1UnitTests
 {
     [Test]
     [TestCase(new[]{ 3, 4, 2, 1, 3, 3}, new[]{ 4, 3, 5, 3, 9, 3}, 11)]
     public void FindTotalDistance_ShouldFindDistance(int[] list1, int[] list2, int expected)
     {
         var result = Day1.FindTotalDistance(list1.ToList(), list2.ToList());
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    [TestCase(new[]{ 3, 4, 2, 1, 3, 3}, new[]{ 4, 3, 5, 3, 9, 3}, 31)]
+    public void FindSimilarityScore_ShouldFindSimilarityScore(int[] list1, int[] list2, int expected)
+    {
+        var result = Day1.FindSimilarityScore(list1.ToList(), list2.ToList());
         Assert.That(result, Is.EqualTo(expected));
     }
 }
--- AdventOfCode2024/Day1.cs
+++ AdventOfCode2024/Day1.cs
@@ -2,14 +2,6 @@
 
 public class Day1
 {
-    /**
-     * Maybe the lists are only off by a small amount!
-     * To find out, pair up the numbers and measure how far apart they are.
-     * Pair up the smallest number in the left list with the smallest number in the right list, then the second-smallest left number with the second-smallest right number, and so on.
-     *
-     * To find the total distance between the left list and the right list, add up the distances between all of the pairs you found.
-     */
-
     public static (List<int>, List<int>) ParseInputFile()
     {
         var input = File.ReadAllText("Resources/Day1Input.txt");
@@ -27,6 +19,14 @@ public class Day1
         
         return (leftList, rightList);
     }
+    
+    /**
+     * Maybe the lists are only off by a small amount!
+     * To find out, pair up the numbers and measure how far apart they are.
+     * Pair up the smallest number in the left list with the smallest number in the right list, then the second-smallest left number with the second-smallest right number, and so on.
+     *
+     * To find the total distance between the left list and the right list, add up the distances between all of the pairs you found.
+     */
 
     public static int FindTotalDistance(List<int> list1, List<int> list2)
     {
@@ -48,5 +48,22 @@ public class Day1
         }
         
         return totalDistance;
+    }
+    
+    /**
+     * This time, you'll need to figure out exactly how often each number from the left list appears in the right list.
+     * Calculate a total similarity score by adding up each number in the left list after multiplying it by the number of times that number appears in the right list.
+     */
+    public static int FindSimilarityScore(List<int> leftList, List<int> rightList)
+    {
+        // First fetch all the unique numbers in the leftList
+        
+        // Then, search through the rightlist
+        
+        // Keep track of how many times each left number appears
+        
+        // Then, multiply each unique leftlist number by their appearances in rightlis
+        
+        // Sum the results and return them
     }
 }
