15646ae90b21ab500929e5cc40dec92fba0bb525
DWF <dwfonderstal@pm.me>
2025-10-21 20:46:21 +0200

    Testcase, pseudocode

---
+++ AdventOfCode2024.UnitTests/Day2UnitTests.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode2024.UnitTests;
+
+public class Day2UnitTests
+{
+    private List<List<int>> _testReport = new List<List<int>>()
+    {
+        new List<int>(){ 7, 6, 4, 2, 1 },   
+        new List<int>(){ 1, 2, 7, 8, 9 },
+        new List<int>(){ 9, 7, 6, 2, 1 },
+        new List<int>(){ 1, 3, 2, 4, 5 },
+        new List<int>(){ 8, 6, 4, 4, 1 },
+        new List<int>(){ 1, 3, 6, 7, 9 },
+    };
+
+    [Test]
+    public void CountSafeLevels_ShouldCountSafeLevels()
+    {
+        var result = Day2.CountSafeLevels(_testReport);
+        Assert.That(result, Is.EqualTo(2));
+    }
+}
---
+++ AdventOfCode2024/Day2.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode2024;
+
+public class Day2
+{
+    /**
+     * The unusual data (your puzzle input) consists of many reports, one report per line.
+     * Each report is a list of numbers called levels that are separated by spaces.
+     * ...
+     * So, a report only counts as safe if both of the following are true:
+     * The levels are either all increasing or all decreasing.
+     * Any two adjacent levels differ by at least one and at most three.
+     */
+    public static int CountSafeLevels(List<List<int>> report)
+    {
+        // Loop through the levels in the report
+        
+        // Determine if the first and second element have a increasing or decreasing distance
+        
+        // Then, keep track if all next steps in the level:
+        // - Are within the 1 3 range
+        // - Are all increasing/decreasing like the first
+        
+        // If they are, add one to our return value
+    }
+}
