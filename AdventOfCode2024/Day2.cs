54359f571294d25e81000e5163460e01fbb4e1b6
DWF <dwfonderstal@pm.me>
2025-10-21 21:26:32 +0200

    Day2L making things a bit simpler with linq query, seperate method

--- AdventOfCode2024/Day2.cs
+++ AdventOfCode2024/Day2.cs
@@ -23,41 +23,38 @@ public class Day2
      */
     public static int CountSafeReports(List<List<int>> input)
     {
-        var safeReports = 0;
-        
         // Loop through the levels in the report
-        foreach (var report in input)
+        return input.Count(ReportIsSafe);
+    }
+
+    private static bool ReportIsSafe(List<int> report)
+    {
+        // We can skip the level if the first two are equal
+        if(report[0] == report[1])
+            return false;
+            
+        var reportIsSafe = true;
+            
+        // Determine if the first and second element have a increasing or decreasing distance
+        var shouldIncrease = report[0] < report[1];
+        var previousLevel = report[0];
+            
+        foreach (var level in report.Skip(1))
         {
-            // We can skip the level if the first two are equal
-            if(report[0] == report[1])
-                continue;
-            
-            var reportIsSafe = true;
-            
-            // Determine if the first and second element have a increasing or decreasing distance
-            var shouldIncrease = report[0] < report[1];
-            var previousLevel = report[0];
-            
-            foreach (var level in report.Skip(1))
+            // Then, keep track if all next steps in the level:
+            // - Are within the 1 3 range
+            // - Are all increasing/decreasing like the first
+            if ((shouldIncrease && previousLevel >= level)
+                || (!shouldIncrease && previousLevel <= level)
+                || (Math.Abs(previousLevel - level) > 3))
             {
-                // Then, keep track if all next steps in the level:
-                // - Are within the 1 3 range
-                // - Are all increasing/decreasing like the first
-                if ((shouldIncrease && previousLevel >= level)
-                    || (!shouldIncrease && previousLevel <= level)
-                    || (Math.Abs(previousLevel - level) > 3))
-                {
-                    reportIsSafe = false;
-                    break;
-                }
-                
-                previousLevel = level;
+                reportIsSafe = false;
+                break;
             }
-            
-            // If the level satisfies our condition, add one to our return value
-            if(reportIsSafe) safeReports++;
+                
+            previousLevel = level;
         }
-        
-        return safeReports;
+
+        return reportIsSafe;
     }
 }
